e2fd94a1b7081db62413295b4e2af6efc2a0db79
rubi-gj <rubengjata@gmail.com>
2026-01-19 17:50:05 +0100

    Models
    This are the models of Job, ApplyFormModel and the model if any error comes.

---
+++ Job Recruiting System/Job Recruiting System/Models/ApplyJobViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Job_Recruiting_System.Models
+{
+    public class ApplyJobViewModel
+    {
+        public string Name { get; set; }
+        public string Field { get; set; }
+        public string CV { get; set; }
+    }
+}
