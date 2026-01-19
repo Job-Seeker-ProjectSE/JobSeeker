7ccbef6e54c9a029576ed8058d43a1ae3cf52b7f
Baci Xhindi <148718785+BaciXhindi123@users.noreply.github.com>
2026-01-19 17:51:29 +0100

    User Models
    The models of Client, model of company and the model of user

---
+++ Job Recruiting System/Job Recruiting System/Models/ClientProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace Job_Recruiting_System.Models
+{
+    public class ClientProfile
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string FullName { get; set; }
+        public int? Age { get; set; }
+        public string Gender { get; set; }
+    }
+
+}
---
+++ Job Recruiting System/Job Recruiting System/Models/CompanyProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace Job_Recruiting_System.Models
+{
+    public class CompanyProfile
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string CompanyName { get; set; }
+        public string Industry { get; set; }
+        public string Website { get; set; }
+    }
+
+}
